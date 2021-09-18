b714d231b53ee8e73e4fd582becb67e8eea4b095
tranhuy <tranquochuy221096@gmail.com>
2021-09-18 16:00:24 +0700

    config babel

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 const express = require ('express')
 const app = express()
-
+const {sort} = require('*/utilities/sort')
 
 const port = process.env.PORT || 3000
 
 app.get ('/', (req, res) => {
     res.send('<h1>hello world</h1>')
 })
+
 
 app.listen(port, () => {
     console.log(`server is running on port ${port}`)
