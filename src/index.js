c844084d26327de76a0280c672e1e19186381a17
tranhuy <tranquochuy221096@gmail.com>
2021-09-18 21:36:37 +0700

    designdb, connect to mongo, add method

--- src/index.js
+++ src/index.js
@@ -1,16 +1,30 @@
 const express = require ('express')
-const app = express()
+const BoardModel = require ('*/models/board.model')
+
 const { connectDB } = require('*/config/mongodb')
 require('dotenv').config()
 
-connectDB()
-
 const port = process.env.PORT || 3000
 
-app.get ('/', (req, res) => {
-    res.send('<h1>hello world</h1>')
-})
+connectDB()
+    .then(() => console.log('connected to DB'))
+    .then(() => bootServer())
+    .catch(error => {
+        console.error(error)
+        process.exit()
+    })
 
-app.listen(port, () => {
-    console.log(`server is running on port ${port}`)
-})
+const bootServer = () => {
+    const app = express()
+    app.get ('/test', async(req, res) => {
+        let fakeData = {
+            title: 'title1'
+        }
+        await BoardModel.createNew(fakeData)
+        res.send('<h1>hello world</h1>')
+    })
+    app.listen(port, () => {
+        console.log(`server is running on port ${port}`)
+    })
+}
+
---
+++ src/models/card.model.js
@@ -0,0 +1,29 @@
+const Joi = require('joi')
+const { getDB } = require('*/config/mongodb')
+
+
+//define
+const cardCollectionName = 'cards'
+const cardCollectionSchema = Joi.object({
+    boardId: Joi.string().required(),
+    columnId: Joi.string().required(),
+    title: Joi.string().required().min(3).max(20),
+    cover: Joi.string().default(null),
+    createdAt: Joi.date().timestamp().default(Date.now()),
+    updatedAt: Joi.date().timestamp().default(null),
+    _destroy: Joi.boolean().default(false)
+})
+const validateSchema = async (data) => {
+    return await cardCollectionSchema.validateAsync(data, { abortEarly: false })
+}
+const createNew = async (data) => {
+    try {
+        const value =await validateSchema(data)
+        const result = await getDB().collection(cardCollectionName).insertOne(value)
+        return result
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+module.exports = { createNew }
